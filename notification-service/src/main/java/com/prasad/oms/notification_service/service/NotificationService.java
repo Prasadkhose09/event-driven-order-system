90840de3b42d4e9690af5f7aebfc615b336dec81
Prasadkhose09 <Prasadkhose512@gmail.com>
2026-03-27 19:43:17 +0530

    Real Email send after placing order

--- notification-service/src/main/java/com/prasad/oms/notification_service/service/NotificationService.java
+++ notification-service/src/main/java/com/prasad/oms/notification_service/service/NotificationService.java
@@ -1,24 +1,26 @@
 package com.prasad.oms.notification_service.service;
 
 import com.prasad.oms.notification_service.dto.OrderEvent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationService {
 
+    @Autowired
+    private EmailService emailService;
+
     public void sendNotification(OrderEvent order) {
 
-        System.out.println("📩 Sending notification...");
-        System.out.println("User " + order.getUserId() +
-                " placed order worth ₹" + order.getTotalPrice());
+        String to = "chrismarti7889@gmail.com"; // change this
+        String subject = "Order Confirmation";
 
-        // simulate delay / processing
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        String body = "Hello,\n\nYour order has been placed successfully.\n\n"
+                + "Product ID: " + order.getProductId() + "\n"
+                + "Quantity: " + order.getQuantity() + "\n"
+                + "Total Amount: ₹" + order.getTotalPrice() + "\n\n"
+                + "Thank you!";
 
-        System.out.println("✅ Notification Sent Successfully!");
+        emailService.sendEmail(to, subject, body);
     }
 }
