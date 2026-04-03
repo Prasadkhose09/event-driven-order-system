bfb1c4912ea04cfd957af0dccc51954021048c7b
Prasadkhose09 <Prasadkhose512@gmail.com>
2026-04-03 09:57:02 +0530

    Containerized all services

--- user-service/src/main/java/com/prasad/oms/user_service/entity/User.java
+++ user-service/src/main/java/com/prasad/oms/user_service/entity/User.java
@@ -1,13 +1,11 @@
 package com.prasad.oms.user_service.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
+@Table(name = "users")
 @Data
 public class User {
 
