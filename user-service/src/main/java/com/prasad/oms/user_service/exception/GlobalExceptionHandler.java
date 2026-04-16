401b62b3a4b1533d38f78cac6e512576fe9216bb
Prasadkhose09 <Prasadkhose512@gmail.com>
2026-04-16 15:14:00 +0530

    Added Error Codes

--- user-service/src/main/java/com/prasad/oms/user_service/exception/GlobalExceptionHandler.java
+++ user-service/src/main/java/com/prasad/oms/user_service/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.prasad.oms.user_service.exception;
 
 
+import com.prasad.oms.user_service.common.ApiResponse;
+import com.prasad.oms.user_service.common.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -17,12 +19,12 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserAlreadyExistException.class)
     public ResponseEntity<Object> handleUserExists(UserAlreadyExistException ex){
-        return buildResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        return buildResponse(ex.getMessage(),"USER_ALREADY_EXISTS", HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<Object> handleUserNotFound(UserNotFoundException ex) {
-        return buildResponse(ex.getMessage(), HttpStatus.NOT_FOUND);
+        return buildResponse(ex.getMessage(), "USER_NOT_FOUND",HttpStatus.NOT_FOUND);
     }
 
 
@@ -40,18 +42,18 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleGeneric(Exception ex){
 
-        return buildResponse("Something went wrong", HttpStatus.INTERNAL_SERVER_ERROR);
+        return buildResponse("Something went wrong","INTERNAL_ERROR", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    private ResponseEntity<Object> buildResponse(String message, HttpStatus status) {
+    private ResponseEntity<Object> buildResponse(String message, String code, HttpStatus status) {
 
-        Map<String, Object> body = new HashMap<>();
+        ApiResponse<Object> response = new ApiResponse<>(
+                false,
+                null,
+                new ErrorResponse(code, message));
 
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", status.value());
-        body.put("message", message);
 
-        return new ResponseEntity<>(body,status);
+        return new ResponseEntity<>(response,status);
     }
 
 }
